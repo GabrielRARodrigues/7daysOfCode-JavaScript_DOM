bd0562f049e2cc13697ef4f8e1d34866874cd01f
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-03-20 11:39:27 -0300

    Adicionando validações básicas nos campos de formulário

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ form.addEventListener('submit', event => {
   event.preventDefault()
   const formDatas = {}
   formFields.forEach(field => {
-    formDatas[field.name] = field.value
+    if (field.name === 'birth-date') {
+      formDatas[field.name] = formatDate(field.value)
+    } else {
+      formDatas[field.name] = field.value
+    }
   })
   console.log(formDatas)
 })
+
+function formatDate(date) {
+  const birthDateObject = new Date(date)
+  const stringLocalDateFormated = `${birthDateObject.getDate() + 1}/${
+    birthDateObject.getMonth() + 1
+  }/${birthDateObject.getFullYear()}`
+  return stringLocalDateFormated
+}
